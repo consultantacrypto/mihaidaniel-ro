55b712453b98f64b156ac43d8c32c1d48d065d29
consultantacrypto <74976300+consultantacrypto@users.noreply.github.com>
2025-11-30 19:40:29 +0200

    Fix Webpack externals for WalletConnect

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,26 @@
 import type { NextConfig } from "next";
 
-const nextConfig: any = { 
-  /* config options here */
+const nextConfig: any = {
+  /* Config options here */
   typescript: {
-    // Ignorăm erorile de TS la build ca să urcăm site-ul
+    // Ignoră erorile de TS la build (ne trebuie viteză acum)
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Ignorăm erorile de stil la build
+    // Ignoră erorile de stil
     ignoreDuringBuilds: true,
+  },
+  // AICI E FIX-UL PENTRU PIERDEREA DE MODULE WALLETCONNECT
+  webpack: (config: any) => {
+    config.externals.push(
+      "pino-pretty",
+      "lokijs",
+      "encoding",
+      "tap",        // Astea sunt cele care dadeau eroare in log
+      "desm",
+      "fastbench"
+    );
+    return config;
   },
 };
 
